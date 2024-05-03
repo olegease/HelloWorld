ef54886bc059d116d573bed84df5286bfcc88ee2
Oleg'Ease'Kharchuk <olegease@gmail.com>
2024-05-03 11:40:05 +0300

    add tests

---
+++ tests/test-HelloWorld.c
@@ -0,0 +1,24 @@
+#include "../source/HelloWorld.h"
+#include <son8/c-file.h>
+#include <son8/c-string.h>
+
+#define BUFFER_SIZE 64
+
+int main()
+{
+    char buffer[BUFFER_SIZE];
+
+    char const *testfile = "test-HelloWorld.txt";
+
+    freopen(testfile, "w", stdout);
+    HelloWorld();
+    fflush(stdout);
+
+    FILE *tmp = fopen(testfile, "r");
+    fgets(buffer, BUFFER_SIZE, tmp);
+    fclose(tmp);
+
+    buffer[BUFFER_SIZE - 1] = '\0';
+
+    return strcmp(buffer, "Hello, World!\n") != 0;
+}
